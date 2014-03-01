a59b3dab0fb8c87bd0023ca4750a20c441500827
Michał Ostrowski <ostrowski.michal@gmail.com>
2014-03-01 21:54:40 +0100

    improve connection to test database

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,24 @@ ENV['RACK_ENV'] = 'test'
 require 'minitest/autorun'
 require 'rack/test'
 
-Oriental::Connection.connect(host: 'localhost', port: 2424, db: 'GratefulDeadConcerts', db_user: "admin", db_password: "admin", storage: "plocal")
+module TestHelper
+  SERVER        = { host: 'localhost', port: 2424 }
+  SERVER_USER   = { user: 'root', password: 'root' }
+
+  TEST_DB       = { db: 'oriental-test', storage: "memory" }
+  TEST_DB_USER  = { user: 'admin', password: 'admin' }
+end
+
+server = OrientdbBinary::Server.new(TestHelper::SERVER)
+server.connect(TestHelper::SERVER_USER)
+
+if server.db_exists? TestHelper::TEST_DB[:db]
+  server.db_drop TestHelper::TEST_DB[:db], TestHelper::TEST_DB[:storage]
+end
+server.db_create(TestHelper::TEST_DB[:db], 'document', TestHelper::TEST_DB[:storage])
+server.disconnect
+Oriental::Connection.connect(host: 'localhost', port: 2424, db: 'oriental-test', db_user: "admin", db_password: "admin", storage: "memory")
+  
 
 class OUser < Struct.new(:parameters)
 end
