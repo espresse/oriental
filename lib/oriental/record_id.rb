f860e4ce9daebb0be00e527c9889842c06f55c1d
Michal Ostrowski <ostrowski.michal@gmail.com>
2014-03-04 15:40:32 +0100

    clean recordid

--- lib/oriental/record_id.rb
+++ lib/oriental/record_id.rb
@@ -1,10 +1,3 @@
 module Oriental
   class RecordId < Orientdb::RecordId; end
-
-  class Rid < Virtus::Attribute
-    def coerce(value)
-      value.is_a?(::Orientdb::RecordId) ? Oriental::RecordId.new(value) : value
-      value.is_a?(::String) ? Oriental::RecordId.new(value) : value
-    end
-  end
 end
