7ed7afae53b6db65c7570b6b261f376afad77a19
Michal Ostrowski <ostrowski.michal@gmail.com>
2014-03-04 16:27:46 +0100

    update document

--- lib/oriental/document.rb
+++ lib/oriental/document.rb
@@ -1,10 +1,13 @@
+require_relative 'field'
+
 module Oriental
   module Document
     extend ActiveSupport::Concern
+    include Inspector
 
     included do
-      include Virtus::Model
-      include Oriental::Attributes::Base
+      include Oriental::Fields
+      attr_reader :fields
     end
 
     module ClassMethods
@@ -13,25 +16,13 @@ module Oriental
       end
     end
 
-    def initialize(record = {})
-      unless record.empty?
-        record[:properties] = record.clone
-          
-        map = record.map do |k, v|
-          k = k.to_s[1..-1].to_sym if k.to_s[0] == "@"
-          k = :klass if k == :class
-          [k, v]
-        end
-          
-        record = Hash[map]
-      end
-
-      super record
+    def initialize(**record)
+      @_dynamic_fields = {}
+      initialize_fields(record)
     end
 
-    def new?
+    def new_record?
       !rid
     end
-
   end
 end
